77da25824ee56301af4d4a7e8321e4cea86f5f5f
Pawan Singh Sodha <sodhapawan421@gmail.com>
2025-04-05 16:59:46 +0530

    dsa files

---
+++ about.java
@@ -0,0 +1,11 @@
+//THIS FILE HAS BEEN FIXED BY PAWAN
+import java.util.Scanner;
+public class about{
+    public static void main(String[] args) {
+        System.out.println("OK HERE");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+        System.out.println(a);
+    }
+}
